18a8dd4ae30707f166a1ab7dd8304847b9a09893
janewysoki <jane.wysoki@gmail.com>
2021-03-16 17:40:45 -0400

    complete functions in books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,21 +1,29 @@
 class Api::V1::BooksController < ApplicationController
     before_action :set_list
 
-    def index #only want books associated with specifc RL?
+    def index #only want books associated with specifc RL
         @books = @reading_list.books
         render json: @books
     end
 
     def create 
-       
+       @book = @reading_list.books.build(book_params)
+       if @book.save
+        render json: @book
+       else
+        render json: {error: 'Error creating book.'}
+       end
     end
 
     def show 
-       
+       @book = Book.find(params[:id])
+       render json: @book
+       #could also just have: @book = @reading_list.books.find_by(id: params[:id])
     end
 
     def destroy
-       
+        #set_list
+        #@book.destroy
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :reading_lists do 
-        resources :books
+        resources :books #now we no longer can access ap1/v1/books so books has to be accessed through reading lists
       end
     end
   end
